30e4d18c837ff3d504589953d6126db53859f548
moznion <moznion@gmail.com>
2014-03-06 00:18:50 +0900

    Impl basic func

--- lib/OAuth1TokenVendingServer/Web/Dispatcher.pm
+++ lib/OAuth1TokenVendingServer/Web/Dispatcher.pm
@@ -3,27 +3,65 @@ use strict;
 use warnings;
 use utf8;
 use Amon2::Web::Dispatcher::RouterBoom;
+use OAuth::Lite::Consumer;
+use OAuth::Lite::Token;
+use OAuth1TokenVendingServer;
 
-any '/' => sub {
+my $config = OAuth1TokenVendingServer->load_config;
+
+my $app_name     = $config->{app_name};
+my $description  = $config->{description};
+my $callback_url = $config->{callback_url};
+my $scope        = join(',', @{$config->{scope}});
+
+my $consumer    = OAuth::Lite::Consumer->new(
+    consumer_key       => $config->{consumer_key},
+    consumer_secret    => $config->{consumer_secret},
+    site               => $config->{site},
+    request_token_path => $config->{request_token_path},
+    access_token_path  => $config->{access_token_path},
+    authorize_path     => $config->{authorize_path},
+);
+
+get '/' => sub {
     my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
+
     return $c->render('index.tx', {
-        counter => $counter,
+        app_name    => $app_name,
+        description => $description,
     });
 };
 
-post '/reset_counter' => sub {
-    my $c = shift;
-    $c->session->remove('counter');
-    return $c->redirect('/');
+get '/auth' => sub {
+    my ($c) = @_;
+
+    my $request_token = $consumer->get_request_token(
+        callback_url => $callback_url,
+        scope        => $scope,
+    ) or die $consumer->errstr;
+
+    $c->session->set(request_token => $request_token->as_encoded);
+    $c->redirect($consumer->url_to_authorize(token => $request_token));
 };
 
-post '/account/logout' => sub {
+get '/token' => sub {
     my ($c) = @_;
-    $c->session->expire();
-    return $c->redirect('/');
+
+    my $verifier      = $c->req->param('oauth_verifier');
+    my $request_token = OAuth::Lite::Token->from_encoded($c->session->get('request_token'));
+
+    my $access_token = $consumer->get_access_token(
+        token    => $request_token,
+        verifier => $verifier,
+    ) or die $consumer->errstr;
+
+    $c->session->expire;
+
+    return $c->render('token.tx', {
+        app_name    => $app_name,
+        token        => $access_token->token,
+        token_secret => $access_token->secret
+    });
 };
 
 1;
