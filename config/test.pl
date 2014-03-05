0a29c815f0d0cb099055007955d89496e694ba0a
moznion <moznion@gmail.com>
2014-03-06 01:00:03 +0900

    Add sample config

--- config/test.pl
+++ config/test.pl
@@ -1,12 +1,20 @@
-use File::Spec;
-use File::Basename qw(dirname);
-my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
-my $dbpath = File::Spec->catfile($basedir, 'db', 'test.db');
-+{
-    'DBI' => [
-        "dbi:SQLite:dbname=$dbpath", '', '',
-        +{
-            sqlite_unicode => 1,
-        }
-    ],
+use strict;
+use warnings;
+use utf8;
+
+return +{
+    app_name    => '__YOUR_APP_NAME__',
+    description => '__YOUR_APP_DESCRIPTION__',
+
+    consumer_key    => '__YOUR_APP_CONSUMER_KEY__',
+    consumer_secret => '__YOUR_APP_CONSUMER_SECRET__',
+
+    site               => 'http://oauth_provider.com',
+    request_token_path => '/request_token',
+    access_token_path  => '/access_token',
+    authorize_path     => '/authorize',
+
+    scope => [qw/read write/],
+
+    callback_url => 'http://YOUR_APP_URL.com/token',
 };
